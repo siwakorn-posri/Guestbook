f9db082aa75309266f8b6661eed63e31db0e15fa
siwakorn-posri <siwakornposrimuang@gmail.com>
2025-10-20 19:45:12 +0700

    Create guestbook migration

---
+++ migrations/m251020_110815_create_guestbook_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%guestbook}}`.
+ */
+class m251020_110815_create_guestbook_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('guestbook', [
+            'id' => $this->primaryKey(),              
+            'name' => $this->string()->notNull(),     
+            'email' => $this->string()->notNull(),    
+            'message' => $this->text()->notNull(),    
+            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'), 
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%guestbook}}');
+    }
+}
